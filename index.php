9cdf3983d126b61b851cd1b9371b0a1818ebfb3e
Sérgio Monteiro Jr. <sergio@publixcomunicacao.com.br>
2014-11-27 10:47:47 -0200

    Mudanças

--- index.php
+++ index.php
@@ -3,11 +3,21 @@
 
 <head>
 	<meta charset="UTF-8">
-	<title>Mendo de Souza</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Mendo de Souza Advogados Associados</title>
+
 	<link rel="stylesheet" href="css/style.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
 </head>
 
 <body>
+	
 
 	
 </body>
